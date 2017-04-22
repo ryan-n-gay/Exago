534e1f1a81ab4976b2e83ed58102b71eae278187
Ryan N Gay <rng0502@gmail.com>
2017-04-22 15:38:05 -0500

    Header
    Yeah, maybe this

--- header.php
+++ header.php
@@ -65,15 +65,10 @@
 		<img class="small-header" src="<?php header_image(); ?>" width="767" alt="<?php bloginfo('name'); ?>">
 		<?php endif; ?>
 
+		<?php exago_header_text(); ?>
+		<img class="logo-header" src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" alt="<?php bloginfo('name'); ?>">
+		<<?php endif; ?>
 	</div>
-
-	<?php $exago_has_header = exago_has_header(); ?>
-	<?php if ( $exago_has_header == 'has-header' ) : ?>
-		<div class="header-logo">
-			<?php exago_header_text(); ?>
-			<img class="logo-header" src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" alt="<?php bloginfo('name'); ?>">
-			<<?php endif; ?>
-		</div>
 
 
 	<?php elseif ( $exago_has_header == 'has-shortcode' ) : ?>
