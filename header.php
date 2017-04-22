d817ccb36364524ced21f2ccdb1bf0f0cc595167
Ryan N Gay <rng0502@gmail.com>
2017-04-22 15:57:05 -0500

    Header
    Oops I did it again

--- header.php
+++ header.php
@@ -67,7 +67,6 @@
 
 		<?php exago_header_text(); ?>
 		<img class="logo-header" src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" alt="<?php bloginfo('name'); ?>">
-		<<?php endif; ?>
 	</div>
 
 	<?php elseif ( $exago_has_header == 'has-shortcode' ) : ?>
